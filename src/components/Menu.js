5780d9c9e0868978183c05f0b52395a9360962a5
Todd Albert <todd@bocacode.com>
2021-02-22 20:50:12 -0500

    basic react app with simple components

---
+++ src/components/Menu.js
@@ -0,0 +1,12 @@
+import React from 'react' 
+
+function Menu() {
+  return (
+    <ul>
+      <li>Home</li>
+      <li>Login</li>
+    </ul>
+  )
+}
+
+export default Menu
---
+++ src/scenes/Home.js
@@ -0,0 +1,15 @@
+import React from 'react' 
+import Menu from '../components/Menu'
+import Footer from '../components/Footer'
+
+function Home() {
+  return (
+    <React.Fragment>
+      <Menu />
+      <h1>HOME</h1>
+      <Footer />
+    </React.Fragment>
+    )
+}
+
+export default Home
