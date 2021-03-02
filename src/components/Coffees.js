f5481ca109981b1bcd2ea3a3a7de7043756b9b91
Todd Albert <todd@bocacode.com>
2021-03-01 20:47:20 -0500

    add hot/cold buttons for coffee list

--- src/components/Coffees.js
+++ src/components/Coffees.js
@@ -1,23 +1,30 @@
 import React, { useState, useEffect } from 'react' 
+import { Button } from 'antd'
 
 function Coffees(){
   const [coffeeList, setCoffeeList] = useState([])
+  const [temperature, setTemperature] = useState('hot')
   useEffect(() => {
-    fetch('https://api.sampleapis.com/coffee/hot')
+    fetch('https://api.sampleapis.com/coffee/'+temperature)
       .then(response => response.json())
       .then(data => {
         setCoffeeList(data)
       })
       .catch(error => console.log('Got Error: ', error))
-  }, [])
-  if(!coffeeList){
-    return <p>Loading...</p>
-  }
+  }, [temperature])
+  // return buttons...
+  // then List OR "loading..."
   return (
     <section>
-      {coffeeList.map(coffee => {
-        return <p key={coffee.id}>{coffee.title}</p>
-      })}
+      <h2>Coffees</h2>
+      <Button type="danger" onClick={() => setTemperature('hot')}>Hot</Button>
+      <Button type="primary" onClick={() => setTemperature('iced')}>Cold</Button>
+      {!coffeeList
+        ? <p>Loading...</p>
+        : coffeeList.map(coffee => {
+          return <p key={coffee.id}>{coffee.title}</p>
+        })
+      }
     </section>
   )
 }
